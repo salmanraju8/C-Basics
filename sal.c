962f56895b9eaf5d10f605ab0f8a8d70728fc5aa
Salman <92290534+salmanraju8@users.noreply.github.com>
2021-11-08 12:01:37 +0530

    Add files via upload

---
+++ sal.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{ 
+  int a,b;
+  a=20;
+  b=10;
+  printf("%d and %d",a>b);
+}
---
+++ sal1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	a=20;
+	b=10;
+	printf("%d > %d -->%d ",a,b,a>b);
+	printf("\n%d>=%d-->%d",a,b,a>=b);
+	printf("\n%d<%d-->%d",a,b,a<b);
+	printf("\n%d<=%d-->%d",a,b,a<=b);
+	printf("\n%d==%d-->%d",a,b,a==b);
+	printf("\n%d!=%d-->%d",a,b,a!=b);
+}
